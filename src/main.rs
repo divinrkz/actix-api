5643e3e1fb406e0750a83e46eb974ee7a1fbcaaf
DivinIrakiza <irakizadivin@gmail.com>
2021-11-23 20:45:00 +0200

    (chore): Create Status models and web server with actix

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,26 @@
-fn main() {
-    println!("Hello, world!");
+use actix_web::{HttpServer, App, web, Responder};
+use std::io;
+
+mod models;
+use crate::models::Status;
+
+async fn status() -> impl Responder {
+   web::HttpResponse::Ok()
+                .json(Status {status: "Ok".to_string()})
+}
+
+#[actix_rt::main]
+async fn main() -> io::Result<()>{
+
+    println! ("Server running on 127.0.0.1:8000");
+
+    HttpServer::new(|| {
+        App::new()
+            .route("/", web::get().to(status))
+    })
+    .bind("127.0.0.1:8000")?
+    .run()
+    .await
+
+   
 }
